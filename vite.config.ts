9b1cdff1f20a9358eb72db4664ee170d71ffbd85
ShubhamWSL <shubhamcr10@gmail.com>
2025-04-11 20:57:50 +0000

    new thing

--- vite.config.ts
+++ vite.config.ts
@@ -5,11 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/portfolio-shubham-temp',
   server: {
     host: "::",
     port: 8080,
   },
+  base: '/portfolio-shubham-temp',
   plugins: [
     react(),
     mode === 'development' &&
